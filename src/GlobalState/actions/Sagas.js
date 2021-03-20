7e35f28bfd7a345bb50bb11e97120e03e5236ce9
Ashok K <kotakommulaashok22@gmail.com>
2021-03-20 13:16:00 +0530

    first commit

--- src/GlobalState/actions/Sagas.js
+++ src/GlobalState/actions/Sagas.js
@@ -1,7 +1,7 @@
 import { put, takeLatest, all } from 'redux-saga/effects'
 import axios from 'axios'
 
-const runOurAction = function*(city, lat, lon) {
+const runOurAction = function*(city) {
     let remoteData;
     yield  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.payload}&units=metric&appid=3fbb2b31fd3e77c536be64abc677a4d1`).then((res) => {
         remoteData = res.data
